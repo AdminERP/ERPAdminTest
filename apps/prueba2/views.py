46c3805d5c79f4e827bc52368a1cd33ae0066902
josealejolibreros <jose.libreros@correounivalle.edu.co>
2018-10-09 07:39:32 -0500

    Style features added. Messages and notifications added. Logical state change in views. Pending in template.

--- apps/prueba2/views.py
+++ apps/prueba2/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
@@ -20,11 +21,26 @@ def registrar_cargo(request, id=None):
         form = RegistrarCargoForm(request.POST, instance=cargo)
         if form.is_valid():
             form.save()
+            messages.success(request, "El cargo ha sido guardado correctamente.")
             return redirect('consultar_cargo')
+        else:
+            messages.error(request, 'Por favor verificar los campos en rojo.')
     return render(request, 'registrar_cargo.html', {'form': form})
-
-
 
 
 def consultar_cargo(request):
     return render(request, 'consulta_cargos.html', {'lista_cargos': Job.objects.all()})
+
+def desactivar_cargo(request, id):
+    cargo = get_object_or_404(Job, id=id)
+    cargo.estado = False
+    cargo.save()
+    messages.success("El cargo ha sido desactivado correctamente del sistema")
+    return redirect("consultar_cargo")
+
+def activar_cargo(request, id):
+    cargo = get_object_or_404(Job, id=id)
+    cargo.estado = True
+    cargo.save()
+    messages.success("El cargo ha sido activado correctamente en el sistema")
+    return redirect("consultar_cargo")
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\MMV Lab\Desktop\pruebarepo\ERPAdmin\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
